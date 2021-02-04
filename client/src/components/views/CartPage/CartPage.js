656090c539b950bf16d3dca995519899aaf105f7
kollead <kollid33@gmail.com>
2021-02-04 23:55:53 +0900

    cart detail

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {useDispatch} from 'react-redux'
 import {getCartItems} from '../../../_actions/user_actions'
+import UserCardBlock from './Sections/UserCardBlock'
 
-function CartPage() {
+function CartPage(props) {
 
+    const [Total, setTotal] = useState(0)
     const dispatch = useDispatch();
-    useEffect((props) => {
+    useEffect(() => {
 
         //redux UserState Cart 안의 상품 확인
         
-        let cartItem
+        let cartItem = []
         
         if(props.user.userData && props.user.userData.cart){
             if(props.user.userData.cart.length>0){
@@ -18,14 +20,30 @@ function CartPage() {
                 })
 
                 dispatch(getCartItems(cartItem, props.user.userData.cart))
+                .then(response=>{calculateTotal(response.payload)})
             }
         }
     }, [props.user.userData])
 
+    let calculateTotal = (cartDetail)=>{
+        let total=0;
+        cartDetail.map((item)=>{
+            total+=parseInt(item.price, 10)*item.quantity
+        })
+        setTotal(total)
+    }
+
     return (
-        <div>
-            
+        <div style={{width: '85%', margin: '3rem auto'}}>
+            <h1>My Cart</h1>
+            <div>
+                <UserCardBlock products={props.user.cartDetail}/>
+            </div>
+            <div style={{marginTop: '3rem'}}>
+                <h2>Total Amount: ${Total}</h2>
+            </div>
         </div>
+        
     )
 }
 
